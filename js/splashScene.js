3a52e16114e5d170befb3ad2294313cc00c4d4a1
wa hihi <wahihikang@gmail.com>
2025-04-17 13:02:48 +0900

    참고 소스와 똑같이 타이핑

---
+++ js/splashScene.js
@@ -0,0 +1,30 @@
+class SplashScene extends Phaser.Scene {
+  constructor () {
+    super({ key: 'splashScene' })
+
+    this.splashSceneBackgroundImage = null
+  }
+
+  init (data) {
+    this.cameras.main.setBackgroundColor('#ffffff')
+  }
+
+  preload () {
+    console.log('Splash Scene')
+    this.load.image('splashSceneBackground', './assets/splashSceneImage.png')
+  }
+
+  create (data) {
+    this.splashSceneBackgroundImage = this.add.sprite(0, 0, 'splashSceneBackground')
+    this.splashSceneBackgroundImage.x = 1920 / 2
+    this.splashSceneBackgroundImage.y = 1080 / 2
+    }
+
+    update (time, delta) {
+        if (time > 3000) {
+            this.scene.switch('titleScene')
+        }
+    }
+}
+
+export default SplashScene
