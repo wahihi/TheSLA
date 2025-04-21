4addf484d1bc3204f5f3fea90f320d3f276272fd
wa hihi <wahihikang@gmail.com>
2025-04-21 17:00:46 +0900

    villain_1 이미지 추가 및 Title Scene에도 추가

--- js/titleScene.js
+++ js/titleScene.js
@@ -6,6 +6,8 @@ class TitleScene extends Phaser.Scene {
         this.titleSceneBackgroundImage = null
         this.titleSceneText = null
         this.titleSceneTextStyle = { font: '200px Times', fill: '#fde4b9', align: 'center'}
+
+        this.villain_1 = null
     }
 
     init (data) {
@@ -15,6 +17,7 @@ class TitleScene extends Phaser.Scene {
     preload () {
         console.log('Title Scene')
         this.load.image('titleSceneBackground', 'assets/aliens_screen_image.jpg')
+        this.load.image("villain_1",'assets/the.jpg')
     }
 
     create (data) {        
@@ -23,10 +26,18 @@ class TitleScene extends Phaser.Scene {
         this.titleSceneBackgroundImage.y = 1080 /2
 
         this.titleSceneText = this.add.text(1920 / 2, (1080/2) + 350, 'Space Aliens', this.titleSceneTextStyle).setOrigin(0.5)
+
+        this.villain_1 = this.add.sprite(1920/2, 1080/2, 'villain_1').setScale(1.0).setOrigin(0.5)
+        this.villain_1.visible = false
+
+        this.time.delayedCall(1500, () => {
+            this.titleSceneBackgroundImage.visible =false
+            this.villain_1.visible = true
+        }, [], this);
     }
 
     update (time, delta){
-        if (time > 6000){
+        if (time > 7000){
             this.scene.switch('menuScene')
         }
     }
